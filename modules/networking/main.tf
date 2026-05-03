9a165984da04d0354e71992417ce4f915413340f
jerolddraj-oss <jerold.d.raj@gmail.com>
2026-05-03 22:48:43 +0530

    Add NSG and associate to AKS subnet

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -1,3 +1,45 @@
+resource "azurerm_network_security_group" "aks_nsg" {
+  name                = "aks-nsg"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  security_rule {
+    name                       = "Allow-HTTP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-HTTPS"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Deny-All"
+    priority                   = 4096
+    direction                  = "Inbound"
+    access                     = "Deny"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
 resource "azurerm_virtual_network" "hub" {
   name                = "hub-vnet"
   address_space       = ["10.0.0.0/16"]
@@ -24,6 +66,11 @@ resource "azurerm_subnet" "spoke_subnet" {
   resource_group_name  = var.resource_group_name
   virtual_network_name = azurerm_virtual_network.spoke.name
   address_prefixes     = ["10.1.1.0/24"]
+}
+
+resource "azurerm_subnet_network_security_group_association" "aks_assoc" {
+  subnet_id                 = azurerm_subnet.spoke_subnet.id
+  network_security_group_id = azurerm_network_security_group.aks_nsg.id
 }
 
 resource "azurerm_virtual_network_peering" "hub_to_spoke" {
