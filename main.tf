c8f16bae0dc783d481d2b9f941b7b48eb6a232d2
jerolddraj-oss <jerold.d.raj@gmail.com>
2026-05-03 20:31:08 +0530

    Fix root main.tf with multi-region AKS modules

--- main.tf
+++ main.tf
@@ -1,3 +1,29 @@
 provider "azurerm" {
   features {}
 }
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-aks-multiregion"
+  location = "East US"
+}
+
+module "aks_us" {
+  source              = "./modules/aks"
+  cluster_name        = "aks-us"
+  location            = "East US"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+module "aks_ind" {
+  source              = "./modules/aks"
+  cluster_name        = "aks-ind"
+  location            = "Central India"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+module "aks_aus" {
+  source              = "./modules/aks"
+  cluster_name        = "aks-aus"
+  location            = "Australia East"
+  resource_group_name = azurerm_resource_group.rg.name
+}
