e3c1296b553674f5468ed32b82ad7b8cd3ebd17d
jerolddraj-oss <jerold.d.raj@gmail.com>
2026-05-03 20:36:14 +0530

    Attach AKS to subnet

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -5,9 +5,10 @@ resource "azurerm_kubernetes_cluster" "aks" {
   dns_prefix          = var.cluster_name
 
   default_node_pool {
-    name       = "default"
-    node_count = 2
-    vm_size    = "Standard_DS2_v2"
+    name           = "default"
+    node_count     = 2
+    vm_size        = "Standard_DS2_v2"
+    vnet_subnet_id = var.subnet_id
   }
 
   identity {
