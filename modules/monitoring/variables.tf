d0e8fbe689a70584da54794c4bb940310ac6283a
jerolddraj-oss <jerold.d.raj@gmail.com>
2026-05-03 22:43:26 +0530

    Add monitoring variables

---
+++ modules/monitoring/variables.tf
@@ -0,0 +1,12 @@
+variable "location" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "aks_id" {
+  description = "AKS Resource ID"
+  type        = string
+}
