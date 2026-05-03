d22deec702ebe9e4b24e7acbb7e0a2a7de728d75
jerolddraj-oss <jerold.d.raj@gmail.com>
2026-05-03 22:46:00 +0530

    Connect monitoring module to all AKS clusters

--- main.tf
+++ main.tf
@@ -36,3 +36,24 @@ module "aks_aus" {
   resource_group_name = azurerm_resource_group.rg.name
   subnet_id           = module.networking.spoke_subnet_id
 }
+
+module "monitoring_us" {
+  source              = "./modules/monitoring"
+  location            = "East US"
+  resource_group_name = azurerm_resource_group.rg.name
+  aks_id              = module.aks_us.id
+}
+
+module "monitoring_ind" {
+  source              = "./modules/monitoring"
+  location            = "Central India"
+  resource_group_name = azurerm_resource_group.rg.name
+  aks_id              = module.aks_ind.id
+}
+
+module "monitoring_aus" {
+  source              = "./modules/monitoring"
+  location            = "Australia East"
+  resource_group_name = azurerm_resource_group.rg.name
+  aks_id              = module.aks_aus.id
+}
