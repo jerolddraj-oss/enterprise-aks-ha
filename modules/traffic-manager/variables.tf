314a55fdaf25a24d207f0598db08bb30de3ea2f9
jerolddraj-oss <jerold.d.raj@gmail.com>
2026-05-03 22:24:22 +0530

    Add Traffic Manager variables

---
+++ modules/traffic-manager/variables.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+  type = string
+}
+
+variable "us_endpoint" {
+  type = string
+}
+
+variable "ind_endpoint" {
+  type = string
+}
+
+variable "aus_endpoint" {
+  type = string
+}
